8b73e376114d9ac3d78ee4151aeb026132ca21d4
Carlos Casa <duardbr@gmail.com>
2020-08-23 04:55:44 -0300

    [on] cruds rapidos

--- libs/lib-api-core/src/lib/config/database.config.ts
+++ libs/lib-api-core/src/lib/config/database.config.ts
@@ -8,8 +8,8 @@ export class DatabaseConfig implements TypeOrmOptionsFactory {
   constructor(private configService: ConfigService) {}
   createTypeOrmOptions() {
     const dbConfig = this.configService.get('database');
-    console.log('\n\n\n');
-    console.log('=>', dbConfig);
+    // console.log('\n\n\n');
+    // console.log('=>', dbConfig);
     console.log('💡');
 
     return this.configService.get('database');
