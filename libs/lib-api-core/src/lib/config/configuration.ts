2cfc1d4e99d0f95cc918248e8cc37bb73f2513b8
Carlos Aquino <duard@Carloss-iMac.local>
2020-11-18 11:56:53 -0300

    upgrade nx 10.4 - aguardando 11.0

--- libs/lib-api-core/src/lib/config/configuration.ts
+++ libs/lib-api-core/src/lib/config/configuration.ts
@@ -6,13 +6,14 @@ export const configuration = () => ({
 
   jwtSecret: process.env.JWT_SECRET,
   database: {
-    port: Number(process.env.API_BASE_TYPEORM_PORT),
     type: process.env.API_BASE_TYPEORM_TYPE || 'mysql',
-    host: process.env.API_BASE_TYPEORM_HOST,
+    port: Number(process.env.API_BASE_TYPEORM_PORT),
+    synchronize: process.env.API_BASE_TYPEORM_SYNC,
+
+    database: process.env.API_BASE_TYPEORM_DATABASE,
     username: process.env.API_BASE_TYPEORM_USERNAME,
     password: process.env.API_BASE_TYPEORM_PASSWORD,
-    database: process.env.API_BASE_TYPEORM_DATABASE,
-    synchronize: process.env.API_BASE_TYPEORM_SYNCHRONIZE,
+    host: process.env.API_BASE_TYPEORM_HOSTNAME,
     entities: [
       __dirname + '/../**/*.entity{.ts,.js}',
       'libs/typeorm/src/lib/entity/**/*.entity{.ts,.js}',
@@ -25,3 +26,16 @@ export const configuration = () => ({
     // entities: getMetadataArgsStorage().tables.map(tbl => tbl.target),
   },
 });
+
+/*
+
+  API_BASE_TYPEORM_TYPE: process.env.API_BASE_TYPEORM_TYPE,
+  API_BASE_TYPEORM_PORT: process.env.API_BASE_TYPEORM_PORT,
+  API_BASE_TYPEORM_SYNC: process.env.API_BASE_TYPEORM_SYNC,
+
+  API_BASE_TYPEORM_DATABASE: process.env.API_BASE_TYPEORM_DATABASE,
+  API_BASE_TYPEORM_USERNAME: process.env.API_BASE_TYPEORM_USERNAME,
+  API_BASE_TYPEORM_PASSWORD: process.env.API_BASE_TYPEORM_PASSWORD,
+  API_BASE_TYPEORM_HOSTNAME: process.env.API_BASE_TYPEORM_HOSTNAME,
+
+*/
